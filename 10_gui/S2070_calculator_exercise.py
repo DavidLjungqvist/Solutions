eca8ac1df712bcd5ff0bcfd28c670d8cb573a461
DavidLjungqvist <80721@edu.campusvejle.dk>
2025-09-28 16:59:17 +0200

    .

--- 10_gui/S2070_calculator_exercise.py
+++ 10_gui/S2070_calculator_exercise.py
@@ -18,18 +18,6 @@ from tkinter import ttk
 #         self.entry_1 = tk.Entry(main_window,)
 
 
-
-
-
-main_window = tk.Tk()
-main_window.title("Calculator")
-main_window.geometry("400x400")
-
-
-
-
-
-
 def reset():
     entry_2.delete(0, tk.END)
     entry_1.configure(state='normal')
@@ -43,37 +31,59 @@ def read_left_entry():
     entry_2.configure(state='normal')
     return entry_1.get()
 
+
 def read_right_entry():
     return entry_2.get()
 
 
-
 def choose_operator_add():
-    current_calculation = label_1.cget("text")
+    # current_calculation = label_1.cget("text")
     num_1 = read_left_entry()
+    global operator
+    operator = "+"
     current_calculation = num_1
-    label_1.config(text=str(current_calculation) + "+")
+    label_1.config(text=str(current_calculation))
 
 
-# def choose_operator_subtract():
-#     populate_left_entry()
-#     label_1.config(text=f"{first_numb} -")
-#
-#
-# def choose_operator_divide():
-#     populate_left_entry()
-#     label_1.config(text=f"{first_numb} /")
-#
-#
-# def choose_operator_multiply():
-#     populate_left_entry()
-#     label_1.config(text=current_string)
-#
-#
-def calculate_result_add():
+def choose_operator_subtract():
+    # current_calculation = label_1.cget("text")
+    num_1 = read_left_entry()
+    global operator
+    operator = "-"
+    current_calculation = num_1
+    label_1.config(text=str(current_calculation))
 
 
-    result = float(read_left_entry()) + float(read_right_entry())
+def choose_operator_divide():
+    # current_calculation = label_1.cget("text")
+    num_1 = read_left_entry()
+    global operator
+    operator = "/"
+    current_calculation = num_1
+    label_1.config(text=str(current_calculation))
+
+
+def choose_operator_multiply():
+    # current_calculation = label_1.cget("text")
+    num_1 = read_left_entry()
+    global operator
+    operator = "*"
+    current_calculation = num_1
+    label_1.config(text=str(current_calculation))
+
+
+def calculate_result():
+    if operator == "+":
+        result = float(read_left_entry()) + float(read_right_entry())
+    elif operator == "-":
+        result = float(read_left_entry()) - float(read_right_entry())
+    elif operator == "/":
+        result = float(read_left_entry()) / float(read_right_entry())
+    elif operator == "*":
+        result = float(read_left_entry()) * float(read_right_entry())
+    global current_result
+    current_result = result
+    update_label()
     entry_1.configure(state='normal')
     entry_1.delete(0, tk.END)
     entry_2.delete(0, tk.END)
@@ -81,24 +91,8 @@ def calculate_result_add():
     entry_1.configure(state='readonly')
 
 
-label_1 = tk.Label(main_window, width=20, text="")
-label_1.grid(row=0, column=0, padx=10, pady=4)
-entry_1 = tk.Entry(main_window, width=20)
-entry_1.grid(row=1, column=0, padx=10, pady=4, ipady=4)
-entry_2 = tk.Entry(main_window, width=20, state="readonly")
-entry_2.grid(row=1, column=1, padx=10, pady=4, ipady=4)
-button_1 = tk.Button(main_window, text="+", command=choose_operator_add)
-button_1.grid(row=2, column=0, padx=10, pady=4,)
-# button_2 = tk.Button(main_window, text="-", command=choose_operator_subtract)
-# button_2.grid(row=2, column=1, padx=10, pady=4,)
-# button_3 = tk.Button(main_window, text="/", command=choose_operator_divide)
-# button_3.grid(row=3, column=0, padx=10, pady=4,)
-# button_4 = tk.Button(main_window, text="*", command=choose_operator_multiply)
-# button_4.grid(row=3, column=1, padx=10, pady=4,)
-button_5 = tk.Button(main_window, text="=", command=calculate_result_add)
-button_5.grid(row=2, column=1, padx=10, pady=4,)
-button_6 = tk.Button(main_window, text="=", command=reset)
-button_6.grid(row=3, column=0, padx=10, pady=4,)
+def update_label():
+    label_1.config(text=current_result)
 
 
 def perform_operation(first_numb, second_numb, operation):
@@ -112,6 +106,33 @@ def perform_operation(first_numb, second_numb, operation):
         return first_numb / second_numb
     else:
         return ("Invalid operation")
+
+
+main_window = tk.Tk()
+main_window.title("Calculator")
+main_window.geometry("400x400")
+
+
+label_1 = tk.Label(main_window, width=20, text="")
+label_1.grid(row=0, column=0, padx=10, pady=4)
+entry_1 = tk.Entry(main_window, width=20)
+entry_1.grid(row=1, column=0, padx=10, pady=4, ipady=4)
+entry_2 = tk.Entry(main_window, width=20, state="readonly")
+entry_2.grid(row=1, column=1, padx=10, pady=4, ipady=4)
+button_1 = tk.Button(main_window, text="+", command=choose_operator_add)
+button_1.grid(row=2, column=0, padx=10, pady=4,)
+button_2 = tk.Button(main_window, text="-", command=choose_operator_subtract)
+button_2.grid(row=2, column=1, padx=10, pady=4,)
+button_3 = tk.Button(main_window, text="/", command=choose_operator_divide)
+button_3.grid(row=3, column=0, padx=10, pady=4,)
+button_4 = tk.Button(main_window, text="*", command=choose_operator_multiply)
+button_4.grid(row=3, column=1, padx=10, pady=4,)
+button_5 = tk.Button(main_window, text="=", command=calculate_result)
+button_5.grid(row=4, column=0, padx=10, pady=4,)
+button_6 = tk.Button(main_window, text="reset", command=reset)
+button_6.grid(row=4, column=1, padx=10, pady=4,)
+
+
 
 
 print(perform_operation(12, 3, "*"))
