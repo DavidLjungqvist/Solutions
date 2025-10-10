391f01bf3c660b122e50ebdb55dcbf743b326f5d
DavidLjungqvist <70821@edu.campusvejle.dk>
2025-10-10 11:54:44 +0200

    .

--- 06_challenges/0830_lunar_arithmetic_exercise.py
+++ 06_challenges/0830_lunar_arithmetic_exercise.py
@@ -32,10 +32,40 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 
 
 class Lunar_int():
-    def __init__(self):
-        self.first_number
-        self.second_number
+    def __init__(self, value: int):
+        self.value = value
+        self.lenght = len(str(abs(value)))
 
-    def lunar_addition(self):
+    def __getitem__(self, index):
+        return self.digits[index]
 
-    def lunar_multiplication(self):
+    def __add__(self, other):
+        result = ""
+        if self.lenght >= other.lenght:
+            for digit in range(self.lenght):
+                if digit >= other[digit]:
+                    new_digit = digit
+                else:
+                    new_digit = self[digit]
+                result += new_digit
+        else:
+            for digit in range(other.lenght):
+                if digit >= self[digit]:
+                    new_digit = digit
+                else:
+                    new_digit = other[digit]
+                result += new_digit
+        return result
+
+    # def lunar_addition(self):
+
+    # TRY TO PUT THE CODE IN A FUNCTION AND CALL THE FUNCTION UNDER __add__
+
+
+
+    # def lunar_multiplication(self):
+
+number1 = Lunar_int(120)
+number2 = Lunar_int(315)
+
+number3 = number1 + number2
