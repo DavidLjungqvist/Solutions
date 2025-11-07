398249ce406763e724694b3f459f558d3234a6a5
DavidLjungqvist <70821@edu.campusvejle.dk>
2025-11-07 11:34:59 +0100

    .

--- 50_plusbus/plusbus_gui.py
+++ 50_plusbus/plusbus_gui.py
@@ -28,3 +28,29 @@ frame_customer.grid(row=0, column=0, padx=padx, pady=pady, sticky=tk.N)
 tree_frame_customer = ttk.Frame(frame_customer)
 tree_frame_customer.grid(row=0, column=0, padx=0, pady=pady)
 tree_scroll_customer = ttk.Scrollbar(tree_frame_customer)
+tree_scroll_customer.grid(row=0, column=1, padx=0, pady= pady, sticky='ns')
+tree_customer = ttk.Treeview(tree_frame_customer, yscrollcommand=tree_scroll_customer.set, selectmode="browse")
+tree_customer.grid(row=0, column=0, padx=0, pady=pady)
+tree_scroll_customer.config(command=tree_customer.yview)
+
+tree_customer['columns'] =("id", "surname", "contact_info")
+tree_customer.column("#0", width=0, stretch=tk.NO)
+tree_customer.column("id", anchor=tk.E, width=60)
+tree_customer.column("surname", anchor=tk.W, width=140)
+tree_customer.column("contact_info", anchor=tk.W, width=200)
+tree_customer.heading("#0", text="", anchor=tk.W)
+tree_customer.heading("id", text="Kunde ID", anchor=tk.CENTER)
+tree_customer.heading("surname", text="Efternavn", anchor=tk.CENTER)
+tree_customer.heading("contact_info", text="Kontakt Information", anchor=tk.CENTER)
+tree_customer.tag_configure('oddrow', background=oddrow)
+tree_customer.tag_configure('evenrow', background=evenrow)
+# tree_customer.bind()
+
+controls_frame_customer = tk.Frame(frame_customer)
+controls_frame_customer.grid(row=3, column=0, padx=padx, pady=pady)
+
+
+
+if __name__ == "__main__":
+    # refresh_treeview(tree_customer, pbd.Customer)
+    main_window.mainloop()
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+import time
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+
+def main():
+    toggle_time = 10000
+    is_on = True
+    while True:
+        time = current_milli_time()
+        if toggle(time, toggle_time, is_on):
+            is_on = not is_on
+        print(time)
+        print(is_on)
+
+
+
+def toggle(current_time, toggle_time, condition):
+    truncated_time = current_time // 10
+    truncated_toggle = toggle_time // 10
+    if truncated_time % truncated_toggle == 0 and not condition:
+        return True
+    elif (truncated_time + toggle_time) % truncated_toggle == 0 and condition:
+        return True
+    else:
+        return False
+
+main()
