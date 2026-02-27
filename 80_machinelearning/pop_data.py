01b9918a818859d901c7012dd13464447dd683a9
david <70821@edu.campusvejle.dk>
2026-02-27 08:07:40 +0100

    .

--- 80_machinelearning/pop_data.py
+++ 80_machinelearning/pop_data.py
@@ -21,8 +21,58 @@ print(wide.shape)
 un_df = pd.read_csv("ML1110_World_Population.csv", index_col="CCA3")
 print(un_df.head(25))
 
-combined_df = wide.join(un_df, how="right")
+combined_df = un_df.join(wide, how="right")
 
 print(combined_df.head(25))
 
 combined_df.to_csv("combined_population.csv", index=False)
+
+
+
+
+#NEXT STEPS GUIDE:
+# detect year cols
+import re
+
+year_cols = []
+
+for col in df.columns:
+    match = re.search(r"\b(18|19|20)\d{2}\b", col)
+    if match:
+        year_cols.append(col)
+
+print(year_cols)
+
+# sort numerically
+year_cols_sorted = sorted(
+    year_cols,
+    key=lambda x: int(re.search(r"\b(18|19|20)\d{2}\b", x).group())
+)
+
+print(year_cols_sorted)
+
+#melt using sorted cols
+df_long = df.melt(
+    id_vars=[col for col in df.columns if col not in year_cols],
+    value_vars=year_cols_sorted,
+    var_name="year",
+    value_name="population"
+)
+
+#exract clean numeric year
+df_long["year"] = (
+    df_long["year"]
+    .str.extract(r"(\d{4})")
+    .astype(int)
+)
+
+#sort
+df_long = df_long.sort_values("year")
+
+#plot
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.lineplot(data=df_long, x="year", y="population", hue="country")
+
+plt.show()
