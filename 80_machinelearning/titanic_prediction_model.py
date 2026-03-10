dd27ee3c68b7e94b0c2d70b57b9ed4a94c8c6b7f
david <70821@edu.campusvejle.dk>
2026-03-10 08:05:39 +0100

    .

--- 80_machinelearning/titanic_prediction_model.py
+++ 80_machinelearning/titanic_prediction_model.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 df = pd.read_csv("titanic_train.csv", index_col="PassengerId")
-print(df.sample(10))
+# print(df.sample(10))
+
+# print(df[df["Age"].isna()]["Parch"].value_counts())
+# print(df[df["Age"].isna()]["SibSp"].value_counts().sort_index())
+
+df["Title"] = df["Name"].str.extract(r',\s*([^\.]+)\.')
+
+print(df["Title"].head(10))
