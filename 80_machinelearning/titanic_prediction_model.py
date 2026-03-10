51d8453e81fca39af9a15b81030da78189490445
70821 <70821@edu.campusvejle.dk>
2026-03-10 12:44:56 +0100

    .

--- 80_machinelearning/titanic_prediction_model.py
+++ 80_machinelearning/titanic_prediction_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("titanic_train.csv", index_col="PassengerId")
 # print(df.sample(10))
@@ -8,4 +9,33 @@ df = pd.read_csv("titanic_train.csv", index_col="PassengerId")
 
 df["Title"] = df["Name"].str.extract(r',\s*([^\.]+)\.')
 
-print(df["Title"].head(10))
+print(df["Title"].unique())
+print(df["Title"].value_counts())
+
+title_survival = df.groupby("Title")["Age"].mean().sort_values()
+# print(title_survival)
+
+title_age = df.groupby("Title")["Age"].mean().sort_values()
+# print(title_age)
+
+# title_survival.plot(kind="bar")
+
+# plt.ylabel("Survival Rate")
+# plt.title("Survival Rate by Title")
+# plt.show()
+
+# title_age.plot(kind="bar")
+#
+# plt.ylabel("Median Age")
+# plt.title("Median Age by Title")
+# plt.show()
+
+
+# for title, group in df.groupby("Title"):
+#     print(title)
+#     print(group)
+
+test = pd.read_csv("titanic_test.csv")
+test["Title"] = test["Name"].str.extract(r',\s*([^\.]+)\.')
+print(test["Title"].unique())
+print(test["Title"].value_counts())
