52e489d00e1967ab2966e37a9ee52ce3a1f7c56d
david <70821@edu.campusvejle.dk>
2026-03-11 17:23:57 +0100

    .

--- 80_machinelearning/titanic_prediction_model.py
+++ 80_machinelearning/titanic_prediction_model.py
@@ -1,22 +1,132 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("titanic_train.csv", index_col="PassengerId")
-# print(df.sample(10))
+test = pd.read_csv("titanic_test.csv", index_col="PassengerId")
 
-# print(df[df["Age"].isna()]["Parch"].value_counts())
-# print(df[df["Age"].isna()]["SibSp"].value_counts().sort_index())
 
 df["Title"] = df["Name"].str.extract(r',\s*([^\.]+)\.')
+test["Title"] = test["Name"].str.extract(r',\s*([^\.]+)\.')
 
-print(df["Title"].unique())
-print(df["Title"].value_counts())
 
 title_survival = df.groupby("Title")["Age"].mean().sort_values()
 # print(title_survival)
 
 title_age = df.groupby("Title")["Age"].mean().sort_values()
 # print(title_age)
+
+title_replace_dict = {"Mlle": "Miss", "Mme": "Mrs", "Ms": "Miss"}
+df["Title"] = df["Title"].replace(title_replace_dict)
+test["Title"] = test["Title"].replace(title_replace_dict)
+
+high_status = ["the Countess", "Sir", "Lady", "Don", "Dona", "Jonkheer"]
+above_average_title = ["Dr", "Major", "Col"]
+hardcore_mfs = ["Capt", "Rev"]
+
+df["Title"] = df["Title"].replace(high_status, "High Status")
+df["Title"] = df["Title"].replace(above_average_title, "Improved Status")
+df["Title"] = df["Title"].replace(hardcore_mfs, "Mr")
+
+test["Title"] = test["Title"].replace(high_status, "High Status")
+test["Title"] = test["Title"].replace(above_average_title, "Improved Status")
+test["Title"] = test["Title"].replace(hardcore_mfs, "Mr")
+
+title_map = {title: i for i, title in enumerate(title_survival.index)}
+
+df["Title_encoded"] = df["Title"].map(title_map)
+test["Title_encoded"] = test["Title"].map(title_map)
+
+gender_map = {"male": 0, "female": 1}
+df["Gender_encoded"] = df["Sex"].map(gender_map)
+test["Gender_encoded"] = test["Sex"].map(gender_map)
+
+df["TotalFamily"] = df["SibSp"] + df["Parch"]
+test["TotalFamily"] = test["SibSp"] + test["Parch"]
+
+df["FamilyGroup"] = "Very Large"
+df.loc[df["TotalFamily"] == 0, "FamilyGroup"] = "Alone"
+df.loc[(df["TotalFamily"] > 0) & (df["TotalFamily"] <= 3), "FamilyGroup"] = "Small"
+df.loc[(df["TotalFamily"] > 3) & (df["TotalFamily"] <= 6), "FamilyGroup"] = "Large"
+df = pd.get_dummies(df, columns=["FamilyGroup"])
+
+test["FamilyGroup"] = "Very Large"
+test.loc[test["TotalFamily"] == 0, "FamilyGroup"] = "Alone"
+test.loc[(test["TotalFamily"] > 0) & (test["TotalFamily"] <= 3), "FamilyGroup"] = "Small"
+test.loc[(test["TotalFamily"] > 3) & (test["TotalFamily"] <= 6), "FamilyGroup"] = "Large"
+test = pd.get_dummies(test, columns=["FamilyGroup"])
+
+print(test.shape)
+print(df.shape)
+
+
+test["Survived"] = 0
+combined_df = pd.concat([df, test], sort=False)
+combined_df["Age"] = combined_df["Age"].fillna(
+    combined_df.groupby(["Pclass", "Sex", "Title"])["Age"].transform("mean")
+)
+
+df_train = combined_df.loc[combined_df.index.isin(df.index)].copy()
+df_test = combined_df.loc[combined_df.index.isin(test.index)].copy()
+
+# print(df_train["Age"].isna().sum(), df_test["Age"].isna().sum())
+# print(df_train.shape, df_test.shape)
+
+df_train["Embarked"] = df_train["Embarked"].fillna(
+    df_train["Embarked"].mode()[0]
+)
+df_test["Fare"] = df_test["Fare"].fillna(
+    df_test.groupby("Pclass")["Fare"].transform("mean")
+)
+
+df_train = pd.get_dummies(df_train, columns=["Embarked"])
+df_test = pd.get_dummies(df_test, columns=["Embarked"])
+
+print(df_test.columns, df_train.columns)
+
+drop_cols = ["Name", "Sex", "SibSp", "Parch", "Ticket", "Cabin", "Title", "TotalFamily"]
+
+passenger_ids = df_test.index
+
+df_train = df_train.drop(columns=drop_cols)
+df_test = df_test.drop(columns=drop_cols)
+
+X = df_train.drop("Survived", axis=1)
+y = df_train["Survived"]
+X_test = df_test.drop("Survived", axis=1)
+
+X_train, X_val, y_train, y_val = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=200,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+val_preds = model.predict(X_val)
+print(accuracy_score(y_val, val_preds))
+
+model.fit(X, y)
+
+predictions = model.predict(X_test)
+
+submission = pd.DataFrame({
+    "PassengerId": passenger_ids,
+    "Survived": predictions
+})
+
+submission.to_csv("submission.csv", index=False)
+
+# print(len(passenger_ids) == len(predictions))
+
+
+
+# print(set(df_train.columns) - set(df_test.columns))
+# print(set(df_test.columns) - set(df_train.columns))
 
 # title_survival.plot(kind="bar")
 
@@ -35,7 +145,5 @@ title_age = df.groupby("Title")["Age"].mean().sort_values()
 #     print(title)
 #     print(group)
 
-test = pd.read_csv("titanic_test.csv")
-test["Title"] = test["Name"].str.extract(r',\s*([^\.]+)\.')
-print(test["Title"].unique())
-print(test["Title"].value_counts())
+# print(test["Title"].unique())
+# print(test["Title"].value_counts())
